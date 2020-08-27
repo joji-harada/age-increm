616a202991af25a23b9cdd4eda2e621de60cf80c
George Allison <49288165+joji-harada@users.noreply.github.com>
2020-08-27 16:01:15 -0700

    added reducer.js - no functionality yet

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    age: 21
+};
+
+const reducer = (state = initialState, action) => {
+    const newState = {...state};
+
+    //add funciton here
+
+    return newState;
+};
+
+export default reducer;
